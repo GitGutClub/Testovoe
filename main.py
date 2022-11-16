a668276a89200a5fa010920505006214acd45308
DESKTOP-162P143\CheekiBreeki <gogogosha830@gmail.com>
2022-11-16 18:26:57 +0700

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+
+import web
+
+urls = (
+    '/', 'HelloRekruto',
+)
+app = web.application(urls, globals())
+render = web.template.render('templates/')
+
+
+class HelloRekruto(object):
+
+    def GET(self):
+        i = web.input(name=None, message=None)
+        return render.index(i.name, i.message)
+
+
+if __name__ == "__main__":
+    app.run()
